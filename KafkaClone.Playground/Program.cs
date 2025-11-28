ce81b3af8c732d609a624741ea99af9ca1bff184
Nektarios <NektariosZampetoulakis@outlook.com>
2025-11-28 17:15:59 -0500

    Initial commit

---
+++ KafkaClone.Playground/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using KafkaClone.Storage;
+
+
+string directory = Directory.GetCurrentDirectory();
+string fullPath = Path.Combine(directory, "test.log");
+
+Console.WriteLine($"Saving logs to: {fullPath}");
+
+using LogSegment logSegment = new LogSegment(fullPath);
+
+string test1 = "Hello World";
+string test2 = "Goodbye World";
+
+byte[] data1 = Encoding.UTF8.GetBytes(test1);
+byte[] data2 = Encoding.UTF8.GetBytes(test2);
+
+long position1 = await logSegment.AppendAsync(data1);
+long position2 = await logSegment.AppendAsync(data2);
+
+byte[] readBytes1 = await logSegment.ReadAsync(0);
+byte[] readBytes2 = await logSegment.ReadAsync(position1);
+
+string result1 = Encoding.UTF8.GetString(readBytes1);
+string result2 = Encoding.UTF8.GetString(readBytes2);
+
+Console.WriteLine(result1);
+Console.WriteLine(result2);
---
+++ KafkaClone.Storage/LogSegment.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace KafkaClone.Storage;
+
+public class LogSegment : IDisposable
+{
+
+    private readonly FileStream _fileStream;
+    
+    public LogSegment(string path)
+{
+        _fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+       _fileStream.Position = _fileStream.Length;
+    
+}
+
+        // [Size (4 bytes)] [Message Data]
+        public async Task<long> AppendAsync(byte[] data)
+    {
+
+       byte[] lengthBytes = BitConverter.GetBytes(data.Length);
+
+       await _fileStream.WriteAsync(lengthBytes);
+       await _fileStream.WriteAsync(data);
+
+       long position = _fileStream.Position;
+
+        return position;
+    }
+
+
+    public async Task<byte[]> ReadAsync(long offset)
+    {
+
+        _fileStream.Position = offset;
+
+        byte[] lengthBuffer = new byte[4];
+
+        await _fileStream.ReadExactlyAsync(lengthBuffer);
+
+        int messageLength = BitConverter.ToInt32(lengthBuffer, 0);
+
+        byte[] payload = new byte[messageLength];
+        
+        await _fileStream.ReadExactlyAsync(payload);
+
+        return payload;
+
+    }
+
+    public void Dispose()
+    {
+        _fileStream.Close();
+    }
+
+}
